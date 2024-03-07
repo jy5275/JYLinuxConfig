c6be4be0e7b0c54abbfe2a74e1a8451108763fd9
jy5275 <yanjiangpku@gmail.com>
2024-03-07 22:13:48 +0700

    init

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Linux platform setup
+
+mkdir repos
+mkdir Downloads
+
+# 1. setup git credentials
+cp .gitconfig ~/
+
+# 1. copy config: .bashrc
+# uncomment: force_color_prompt=yes
+
+
+# 2. vimrc, .tmux.conf
+
+# 2. setup git
+
+# 3. sshkey-gen
+
+# 4. apt install
