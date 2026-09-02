aafe09dcc41b701ac4e33e7adb09ba594ef94cb1
Yan Jiang <43395312+jy5275@users.noreply.github.com>
2026-09-02 14:52:28 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -15,4 +15,6 @@ cp .gitconfig ~/
 mkdir -p ~/repos
 mkdir -p ~/Downloads
 
+mkdir -p ~/.config/git
+grep -qxF '.opencode/' ~/.config/git/ignore 2>/dev/null || echo '.opencode/' >> ~/.config/git/ignore
 git config --global core.excludesFile ~/.config/git/ignore
